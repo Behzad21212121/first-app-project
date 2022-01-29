f1f2c16d4dddd93f6bd66eb0877a1f75d967a85c
Behzad2121 <www.behzadrezaei8@gmail.com>
2022-01-29 17:31:43 +0330

    App.js modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h3>Hello, My Name Is Mohammad Borabadi</h3>
+    </div>
+  );
 };
 
 export default App;
